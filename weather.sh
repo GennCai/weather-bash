e0d213841341ee90d8429bc1a7a94308f0244e9b
caigeng <caigeng@nibatech.cn>
2016-10-25 16:29:04 +0800

    提交

---
+++ weather.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+set -e
+
+# Source the `json.sh` library.
+. "lib/json.sh"
+
+url="http://apis.baidu.com/heweather/weather/free"
+apikey="0ae09eed4f3c0267cb6e6ed7e1cdda8f"
+city="xian"
+
+usage(){
+	echo "使用方法：weather [城市汉字名称或拼音]"
+	echo "默认城市：西安"
+}
+
+if [[ $# == 1 ]]; then
+	if [[ $1 = "-h" || $1 = "--help" || $1 = "-help" ]]; then
+		usage
+		exit
+	else
+		city=$1
+	fi
+elif [[ $# > 1 ]]; then
+		usage
+		exit
+fi
+
+weather=`curl --silent --header "apikey:$apikey" -G -d "city=$city" $url`
+
+wTxt=`echo $weather | json | grep "now/cond/txt"`
+
+wTxt=`echo ${wTxt##/H*g}`
+
+echo "今天天气：$wTxt"
+
